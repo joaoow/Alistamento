87ceff6b27bb1a5a880fe2812e49668ea9d6fb82
joaolinux <87071803+joaolinux@users.noreply.github.com>
2022-05-25 19:50:31 -0300

    Alistamento.py

---
+++ strings.py
@@ -0,0 +1,10 @@
+print("Olá jovem, Bem-Vindo ao exercito brasileiro!")
+
+idade = input("Digite sua idade:")
+peso = input("Digite seu peso:")
+altura = input("Digite sua altura:")
+
+if idade >= '18' and peso >= '60' and altura >= '1.70':
+print("Voce foi aprovado pelo exercito")
+else:
+print("Deslcupe, você não foi aprovado pelo exercito")
